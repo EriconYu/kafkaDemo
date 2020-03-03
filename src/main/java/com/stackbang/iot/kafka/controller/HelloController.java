5b1d1059bafc99c9bf7b9216a4891c0e7b084de3
EricFreeman <yu-zhq@qq.com>
2020-03-03 17:01:49 +0800

    Update HelloController.java

--- src/main/java/com/stackbang/iot/kafka/controller/HelloController.java
+++ src/main/java/com/stackbang/iot/kafka/controller/HelloController.java
@@ -1,1 +1,20 @@
-package com.stackbang.iot.kafka.controller;import lombok.extern.slf4j.Slf4j;import org.springframework.http.ResponseEntity;import org.springframework.stereotype.Controller;import org.springframework.web.bind.annotation.GetMapping;import org.springframework.web.bind.annotation.PathVariable;import org.springframework.web.bind.annotation.RequestMapping;@Slf4j@Controller@RequestMapping("/api/v1/hello")public class HelloController {    @GetMapping("/{id}")    public ResponseEntity<Boolean> getHello(@PathVariable String id) {        log.info("id is {}", id);        log.info("dir is {}" , System.getProperty("user.dir"));        return ResponseEntity.ok(true);    }}
+package com.stackbang.iot.kafka.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/api/v1/hello")
+public class HelloController {
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Boolean> getHello(@PathVariable String id) {
+        log.info("id is {}", id);
+        return ResponseEntity.ok(true);
+    }
+}
